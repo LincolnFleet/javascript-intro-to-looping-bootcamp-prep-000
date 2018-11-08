e4185d4fc24907cd41b7c995c508475d79b7f684
Travis Martin <travisemerymartin@gmail.com>
2018-11-08 23:43:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop(array) {
   for (var i = 0, i>25, i++) {
     if (array.length = 2) {
       array.push(`I am 1 strange loop.`)
+    }
     else {
       array.push(`I am ${array.length} strange loops.`)
-    }
     }
   }
   return array
